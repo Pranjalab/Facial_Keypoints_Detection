dfb5b39e646d4fe2a549d67f4a9714f363293255
Pranjal Bhaskare <pranjalab@gmail.com>
2018-10-18 14:57:05 +0530

    Update Load_Network.py

--- Load_Network.py
+++ Load_Network.py
@@ -2,6 +2,7 @@ from keras.models import model_from_json
 from keras import metrics
 from pandas.io.parsers import read_csv
 from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
@@ -36,9 +37,15 @@ def load(train_flag=True):
     X = X.reshape(-1, 96, 96, 1)
     return X, y
 
+def display_pre(data, model):
+    y = model.predict(data)
+    image = data.reshape(96, 96)
+    plt.imshow(image, cmap='gray')
+    plt.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='o', s=10, c='r')
 
-json = 'weights/1000.json'
-h5 = 'weights/1000.h5'
+
+json = 'weights/150.json'
+h5 = 'weights/150.h5'
 
 # Model reconstruction from JSON file
 with open(json, 'r') as f:
@@ -49,6 +56,7 @@ model.load_weights(h5)
 model.compile(optimizer='adagrad', loss='mean_squared_error', metrics=['accuracy', metrics.mean_squared_error])
 
 x_submit, _ = load(False)
+
 
 
 
